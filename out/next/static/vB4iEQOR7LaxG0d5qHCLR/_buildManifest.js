b0bf4519b96ed282f43b272d484094c5415be087
Necfol <necfoll@gmail.com>
2024-03-26 09:59:17 +0800

    refactor(rm out dir): rm

--- out/next/static/vB4iEQOR7LaxG0d5qHCLR/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/671-60bd21cc061364e2.js","static/css/7104bcb5837bd9f4.css","static/chunks/pages/index-c1d62224ab0160db.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
