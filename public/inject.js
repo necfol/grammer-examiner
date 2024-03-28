017f5e82047e3b0a22c75b9646e0d930207b9a2b
Necfol <necfoll@gmail.com>
2024-03-28 22:11:19 +0800

    fix(rm useless permissions): rm useless

--- public/inject.js
+++ public/inject.js
@@ -1,10 +1,16 @@
+/*
+ * @LastEditors: Necfol
+ * @Date: 2024-03-28 21:54:57
+ * @LastEditTime: 2024-03-28 22:09:29
+ * @FilePath: /grammer-examiner/public/inject.js
+ */
 console.log("Message from inject.js")
-let editorExtensionId = 'jjdkahokkklbddcgkbamaodgkhmdknfe'
+let editorExtensionId = ''
 let selectedText = ''
 
 document.addEventListener('mouseup', function () {
   selectedText = document.getSelection().toString()
-  chrome.runtime.sendMessage(editorExtensionId, { text: selectedText },
+  chrome.runtime.sendMessage(null, { text: selectedText },
     function (response) {
       console.log('>>>>', response)
     })
